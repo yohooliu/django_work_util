3cc61a191c5bfdaf838d9edfb47d0d3ab633e4d9
yohooliu <98161184+yohooliu@users.noreply.github.com>
2022-01-21 22:42:34 +0800

    Update page.py

--- util/page.py
+++ util/page.py
@@ -1,6 +1,11 @@
 # Author:yohoo
 class PageInfo(object):
     def __init__(self,current_page,total_count,per_pagecount):
+        """
+        current_page:当前页
+        total_count:总数
+        per_pagecount:每页数量
+        """
         self.current_page = current_page
         self.total_count = total_count
         self.per_pagecount = per_pagecount
